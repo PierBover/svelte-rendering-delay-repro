bcf51efd5a65f20a6183e3a1fc04cb87d57b44d3
Pier Bover <pierbover11@gmail.com>
2021-04-09 15:38:14 -0500

    first commit

---
+++ src/components/App.svelte
@@ -0,0 +1,35 @@
+<script>
+	import {onMount} from 'svelte';
+
+	let showGreen = false;
+
+	setTimeout(() => {
+		showGreen = true;
+	}, 1000);
+
+	onMount(() => {
+		console.log('mounted!');
+	});
+
+</script>
+
+{#if showGreen}
+	<div class="green"></div>
+{:else}
+	<div class="red"></div>
+{/if}
+
+
+<style>
+	.red {
+		background-color: tomato;
+		width: 10rem;
+		height: 10rem;
+	}
+
+	.green {
+		background-color: limegreen;
+		width: 10rem;
+		height: 10rem;
+	}
+</style>
